e96dd903bf9a16b6f0d4e21749daad68a8d79ba5
Masaru Moribe <masaru.aya@gmail.com>
2026-06-10 16:22:46 +0900

    initial scaffold

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ color: 'white', padding: '2rem' }}>
+      <h1>Masaru Moribe</h1>
+      <p>Portfolio coming soon ✦</p>
+    </div>
+  )
+}
+
+export default App
